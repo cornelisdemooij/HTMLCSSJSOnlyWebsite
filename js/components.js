9abfd35e42b2b74d81fe1e2a48637ba61ce029a7
Cornelis de Mooij <codemo@gmail.com>
2020-02-08 23:32:54 +0100

    More clean-up, added email and linkedin icons with links, added CVs, adjusted styling

--- js/components.js
+++ js/components.js
@@ -1,72 +1,8 @@
-function loadHead() {
-  let title = document.createElement('title');
-  title.innerHTML = 'Cornelis de Mooij - Software &amp; Aerospace Engineer';
-  document.head.appendChild(title);
-
-  let charset = document.createElement('meta');
-  charset.setAttribute('charset', 'UTF-8');
-  document.head.appendChild(charset);
-  
-  let description = document.createElement('meta');
-  description.setAttribute('name', 'description');
-  description.setAttribute('content', 'Personal page of Cornelis de Mooij');
-  document.head.appendChild(description);
-
-  let keywords = document.createElement('meta');
-  keywords.setAttribute('name', 'keywords');
-  keywords.setAttribute('content', 'Software, Aerospace, Engineer');
-  document.head.appendChild(keywords);
-
-  let author = document.createElement('meta');
-  author.setAttribute('name', 'author');
-  author.setAttribute('content', 'Cornelis de Mooij');
-  document.head.appendChild(author);
-
-  let stylesheet = document.createElement('link');
-  stylesheet.setAttribute('rel', 'stylesheet');
-  stylesheet.setAttribute('type', 'text/css');
-  stylesheet.setAttribute('href', './css/style.css');
-  document.head.appendChild(stylesheet);
-
-  let icon = document.createElement('link');
-  icon.setAttribute('rel', 'shortcut icon');
-  icon.setAttribute('type', 'image/png');
-  icon.setAttribute('href', './images/favicon.png');
-  document.head.appendChild(icon);
-}
-
 let pages = {
   "Home" : "./index.html",
   "Education" : "./edu.html",
   "Experience" : "./exp.html",
 };
-
-function loadHeader(pageName) {
-  loadBanner();
-  loadName();
-  loadNavigation(pageName);
-}
-
-function loadBanner() {
-  let banner = document.createElement('div');
-  banner.setAttribute('class', 'banner');
-
-  let portrait = document.createElement('img');
-  portrait.setAttribute('class', 'portrait');
-  portrait.setAttribute('src', './images/portrait.png');
-  portrait.setAttribute('alt', 'Cornelis de Mooij');
-  portrait.setAttribute('title', 'Cornelis de Mooij');
-
-  banner.appendChild(portrait);
-  document.body.appendChild(banner);
-}
-
-function loadName() {
-  let name = document.createElement('div');
-  name.setAttribute('class', 'name');
-  name.innerHTML = '<b>Cornelis de Mooij - Aerospace &amp; Software Engineer</b>';
-  document.body.appendChild(name);
-}
 
 function loadNavigation(pageName) {
   let ul = document.createElement('ul');
--- js/ycescape/output.js
+++
@@ -1,8 +0,0 @@
-// Appending to output:
-function appendToOutput(content) {
-  mainMenuLog += content;
-  output.innerHTML = mainMenuLog;
-}
-function appendDivToOutput(content) {
-  appendToOutput("<div>" + content + "</div>");
-}
